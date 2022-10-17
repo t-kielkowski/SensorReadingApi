6e9e53c0a3bc054e1035c480e56639105368ae2b
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-10-17 20:06:19 +0200

    Add services to the container

--- SensorReadingApi/Program.cs
+++ SensorReadingApi/Program.cs
@@ -1,11 +1,23 @@
+using MediatR;
+using SensorReading.Application.Feature.Beehive;
+using SensorReading.InfrastructureChart;
+using SensorReading.InfrastructureChart.Repository;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SensorReadingsContext>();
+builder.Services.AddTransient<IBH1750Repository, BH1750Repository>();
+builder.Services.AddTransient<IBMP280Repository, BMP280Repository>();
+builder.Services.AddTransient<IHDC1080Repository, HDC1080Repository>();
+builder.Services.AddTransient<ISHT31Repository, SHT31Repository>();
+builder.Services.AddTransient<ISHT31TestRepository, SHT31TestRepository>();
+builder.Services.AddTransient<ISoilMoistureRepository, SoilMoistureRepository>();
+builder.Services.AddMediatR(typeof(GetSensorDataQuery));
 
 var app = builder.Build();
 
