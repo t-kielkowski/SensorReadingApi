acd631513ec11f968d5841cef48f2c0cf975c75c
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-15 22:41:44 +0100

    added WeightReadingsSearchParams

---
+++ SensorReading.Application/Dto/WeightReadingsSearchParams.cs
@@ -0,0 +1,11 @@
+﻿using SensorReading.Infrastructure.Repository.WeightReadings;
+
+namespace SensorReading.Application.Dto
+{
+    public class WeightReadingsSearchParams : IWeightReadingsSearchParams
+    {
+        public string WeightId { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+    }
+}
---
+++ SensorReading.Infrastructure/Repository/WeightReadings/IWeightReadingsRepository.cs
@@ -0,0 +1,13 @@
+﻿using SensorReading.Domain.Models;
+using SensorReading.InfrastructureChart.Repository;
+
+namespace SensorReading.Infrastructure.Repository.WeightReadings
+{
+    public interface IWeightReadingsRepository : IBaseRepository<WeightReading>
+    {
+        public Task<IEnumerable<WeightReading>> GetTempByIdAsync(IWeightReadingsSearchParams searchParams);
+        public Task<IEnumerable<WeightReading>> GetMoisByIdAsync(IWeightReadingsSearchParams searchParams);
+        public Task<IEnumerable<WeightReading>> GetWeightByIdAsync(IWeightReadingsSearchParams searchParams);
+        public Task<IEnumerable<WeightReading>> GetBaterryLevelByIdAsync(IWeightReadingsSearchParams searchParams);
+    }
+}
