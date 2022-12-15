b75d7dae92546b352ad4c782aaa7be5fe388ac2b
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-15 22:43:01 +0100

    WeightReadingsRepository

---
+++ SensorReading.Infrastructure/Repository/WeightReadings/IWeightReadingsSearchParams.cs
@@ -0,0 +1,11 @@
+﻿namespace SensorReading.Infrastructure.Repository.WeightReadings
+{
+    public interface IWeightReadingsSearchParams
+    {
+        public string WeightId { get; set; }
+
+        public DateTime? DateFrom { get; set; }
+ 
+        public DateTime? DateTo { get; set; }  
+    }
+}
---
+++ SensorReading.Infrastructure/Repository/WeightReadings/WeightReadingsRepository.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SensorReading.Domain.Models;
+using SensorReading.InfrastructureChart;
+using SensorReading.InfrastructureChart.Repository;
+
+namespace SensorReading.Infrastructure.Repository.WeightReadings
+{
+    public class WeightReadingsRepository : BaseRepository<WeightReading>, IWeightReadingsRepository
+    {
+        public WeightReadingsRepository(SensorReadingsContext dbContext) : base(dbContext)
+        {}
+
+        public async Task<IEnumerable<WeightReading>> GetTempByIdAsync(IWeightReadingsSearchParams searchParams)
+        {
+            var query = DbContext.WeightReadings
+                .Where(x => x.WeightId == searchParams.WeightId);
+
+            if (searchParams.DateFrom.HasValue)            
+                query = query.Where(x => x.ReadingTime >= searchParams.DateFrom);
+
+            if (searchParams.DateTo.HasValue)
+                query = query.Where(x => x.ReadingTime <= searchParams.DateTo);
+
+            var result = await query
+                 .AsNoTracking()
+                .AsSingleQuery()
+                .Select(x=> new WeightReading
+                {
+                    Temperature= x.Temperature,
+                    ReadingTime= x.ReadingTime
+                })
+                .ToListAsync();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<WeightReading>> GetMoisByIdAsync(IWeightReadingsSearchParams searchParams)
+        {
+            var query = DbContext.WeightReadings
+                .Where(x => x.WeightId == searchParams.WeightId);
+
+            if (searchParams.DateFrom.HasValue)
+                query = query.Where(x => x.ReadingTime >= searchParams.DateFrom);
+
+            if (searchParams.DateTo.HasValue)
+                query = query.Where(x => x.ReadingTime <= searchParams.DateTo);
+
+            var result = await query
+                 .AsNoTracking()
+                .AsSingleQuery()
+                .Select(x => new WeightReading
+                {
+                    Moisture = x.Moisture,
+                    ReadingTime = x.ReadingTime
+                })
+                .ToListAsync();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<WeightReading>> GetWeightByIdAsync(IWeightReadingsSearchParams searchParams)
+        {
+            var query = DbContext.WeightReadings
+                .Where(x => x.WeightId == searchParams.WeightId);
+
+            if (searchParams.DateFrom.HasValue)
+                query = query.Where(x => x.ReadingTime >= searchParams.DateFrom);
+
+            if (searchParams.DateTo.HasValue)
+                query = query.Where(x => x.ReadingTime <= searchParams.DateTo);
+
+            var result = await query
+                 .AsNoTracking()
+                .AsSingleQuery()
+                .Select(x => new WeightReading
+                {
+                    Weight = x.Weight,
+                    ReadingTime = x.ReadingTime
+                })
+                .ToListAsync();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<WeightReading>> GetBaterryLevelByIdAsync(IWeightReadingsSearchParams searchParams)
+        {
+            var query = DbContext.WeightReadings
+                .Where(x => x.WeightId == searchParams.WeightId);
+
+            if (searchParams.DateFrom.HasValue)
+                query = query.Where(x => x.ReadingTime >= searchParams.DateFrom);
+
+            if (searchParams.DateTo.HasValue)
+                query = query.Where(x => x.ReadingTime <= searchParams.DateTo);
+
+            var result = await query
+                 .AsNoTracking()
+                .AsSingleQuery()
+                .Select(x => new WeightReading
+                {
+                    BatteryLevel = x.BatteryLevel,
+                    ReadingTime = x.ReadingTime
+                })
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
