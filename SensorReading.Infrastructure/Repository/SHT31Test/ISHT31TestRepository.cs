348b59da09ce527572959afc8850bedc02790880
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2023-01-26 18:20:32 +0100

    removed SHT31 Test

--- SensorReading.Infrastructure/Repository/SHT31Test/ISHT31TestRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using SensorReading.Domain.Models;
-
-namespace SensorReading.InfrastructureChart.Repository
-{
-    public interface ISHT31TestRepository : IBaseRepository<Sht31test>
-    {
-    }
-}
